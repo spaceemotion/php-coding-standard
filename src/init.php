34f4015c5d744727e82b106ee25cba858f4e583a
André Gräßel <hello@spaceemotion.net>
2020-04-27 09:47:24 +0200

    Wrap main logic into anonymous functions
    That way the code does not clutter the global namespace

--- src/init.php
+++ src/init.php
@@ -2,35 +2,35 @@
 
 declare(strict_types=1);
 
-// Find root "bin" folder for composer installation
-$autoloadFile = 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+(static function () {
+    // Find root "bin" folder for composer installation
+    $autoloadFile = 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-foreach (
-    [
-        __DIR__ . DIRECTORY_SEPARATOR . '..',
-        __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..',
-    ] as $path
-) {
-    if (file_exists($path . DIRECTORY_SEPARATOR . $autoloadFile)) {
-        define('PHPCSTD_ROOT', $path . DIRECTORY_SEPARATOR);
+    foreach (
+        [
+            __DIR__ . DIRECTORY_SEPARATOR . '..',
+            __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..',
+        ] as $path
+    ) {
+        if (file_exists($path . DIRECTORY_SEPARATOR . $autoloadFile)) {
+            define('PHPCSTD_ROOT', $path . DIRECTORY_SEPARATOR);
 
-        define(
-            'PHPCSTD_BINARY_PATH',
-            $path
-            . DIRECTORY_SEPARATOR . 'vendor'
-            . DIRECTORY_SEPARATOR . 'bin'
-            . DIRECTORY_SEPARATOR,
-        );
+            define(
+                'PHPCSTD_BINARY_PATH',
+                $path
+                . DIRECTORY_SEPARATOR . 'vendor'
+                . DIRECTORY_SEPARATOR . 'bin'
+                . DIRECTORY_SEPARATOR,
+            );
 
-        break;
+            break;
+        }
     }
-}
 
-if (! defined('PHPCSTD_ROOT')) {
-    fwrite(STDERR, 'Vendor folder not found. Did you forget to run "composer install"?' . PHP_EOL);
-    exit(1);
-}
+    if (!defined('PHPCSTD_ROOT')) {
+        fwrite(STDERR, 'Vendor folder not found. Did you forget to run "composer install"?' . PHP_EOL);
+        exit(1);
+    }
 
-unset($autoloadFile);
-
-require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
+})();
