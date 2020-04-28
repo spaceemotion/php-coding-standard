b4281dfb33db7548920e0b564dc7b39cd6c0c9bd
André Gräßel <hello@spaceemotion.net>
2020-04-28 20:47:40 +0200

    Add result collection support to phpstan

--- src/Tools/Phpstan.php
+++ src/Tools/Phpstan.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Spaceemotion\PhpCodingStandard\Tools;
 
 use Spaceemotion\PhpCodingStandard\Context;
+use Spaceemotion\PhpCodingStandard\Formatter\File;
+use Spaceemotion\PhpCodingStandard\Formatter\Result;
+use Spaceemotion\PhpCodingStandard\Formatter\Violation;
 
 class Phpstan extends Tool
 {
@@ -12,12 +15,40 @@ class Phpstan extends Tool
 
     public function run(Context $context): bool
     {
-        return $this->execute($this->name, [
+        $output = [];
+
+        if ($this->execute($this->name, [
             'analyse',
             ...$context->files,
-            $context->runningInCi
-                ? '--no-ansi --error-format=checkstyle'
-                : '--ansi',
-        ]) === 0;
+            '--error-format=json',
+            '--no-progress',
+            '--no-ansi',
+            '--no-interaction',
+        ], $output) === 0) {
+            return true;
+        }
+
+        $json = json_decode($output[count($output) - 1], true, 512, JSON_THROW_ON_ERROR);
+
+        $result = new Result();
+
+        foreach ($json['files'] as $filename => $details) {
+            $file = new File();
+
+            foreach ($details['messages'] as $message) {
+                $violation = new Violation();
+                $violation->line = $message['line'];
+                $violation->message = $message['message'];
+                $violation->tool = $this->name;
+
+                $file->violations[] = $violation;
+            }
+
+            $result->files[$filename] = $file;
+        }
+
+        $context->addResult($result);
+
+        return false;
     }
 }
