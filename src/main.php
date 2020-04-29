c253b8204b341de9bc517a87b59e1cb46cc9be36
André Gräßel <hello@spaceemotion.net>
2020-04-30 00:55:10 +0200

    Move ECS to the end of the toolchain
    Since previous runs like psalm or phpstan might rewrite the code, we want to fix any code style issues at the end of the run.

--- src/main.php
+++ src/main.php
@@ -9,8 +9,8 @@ require_once 'init.php';
 exit((new Cli($argv))->start([
     new Tools\ComposerNormalize(),
     new Tools\PhpParallelLint(),
-    new Tools\EasyCodingStandard(),
     new Tools\PhpMessDetector(),
     new Tools\Phpstan(),
     new Tools\Psalm(),
+    new Tools\EasyCodingStandard(),
 ]) ? 1 : 0);
