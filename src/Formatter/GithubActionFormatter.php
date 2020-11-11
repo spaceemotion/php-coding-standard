31b1ea543956eddf54d199449bbbb04ba5709f06
André Gräßel <hello@spaceemotion.net>
2020-11-11 15:25:19 +0100

    Refactor github matcher
    - Don't depend on custom matcher config
    - Merge multiple violations on the same line using a newline feed

--- src/Formatter/GithubActionFormatter.php
+++ src/Formatter/GithubActionFormatter.php
@@ -8,16 +8,23 @@ class GithubActionFormatter extends ConsoleFormatter
 {
     public function format(Result $result): void
     {
-        echo '::add-matcher::' . __DIR__ . '/github-matcher.json' . "\n";
-
         foreach ($result->files as $fileName => $file) {
-            echo "::group::{$fileName}\n";
-
             $fullPath = PHPCSTD_ROOT . $fileName;
+            $violations = [];
 
             foreach ($file->violations as $violation) {
                 $type = strtolower($violation->severity);
-                echo "{$fullPath}:{$violation->line} {$type} {$violation->message} ({$violation->tool})\n";
+                $violations[$violation->line][$type][] = "{$violation->message} ({$violation->tool})";
+            }
+
+            echo "::group::{$fileName}\n";
+
+            foreach ($violations as $line => $byType) {
+                foreach ($byType as $type => $violation) {
+                    // Replace NL with url-encoded %0A so they show up
+                    $violation = str_replace("\n", '%0A', trim(implode("\n", $violation)));
+                    echo "::{$type} file={$fullPath},line={$line}::{$violation}\n";
+                }
             }
 
             echo "::endgroup::\n";
